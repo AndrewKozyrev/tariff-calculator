fb068f9cb07b7928670c73253118150072e9f814
Konstantin Shibkov <shibkov.k@gmail.com>
2023-11-06 23:30:28 +0500

    fix ConfigurationPropertiesScan

--- app/src/main/java/ru/fastdelivery/Main.java
+++ app/src/main/java/ru/fastdelivery/Main.java
@@ -9,7 +9,7 @@ import org.springframework.boot.context.properties.EnableConfigurationProperties
  * Класс запускающий приложение
  */
 @SpringBootApplication(scanBasePackages = { "ru.fastdelivery" })
-@ConfigurationPropertiesScan(value = { "ru.fastdelivery.properties_provider" })
+@ConfigurationPropertiesScan(value = { "ru.fastdelivery.properties" })
 @EnableConfigurationProperties
 public class Main {
     public static void main(String[] args) {
