c6e9372e12a75e7b86e6ab396699ac07ea1a6a81
Konstantin Shibkov <shibkov.k@gmail.com>
2023-11-06 21:19:15 +0500

    add swagger schema description

--- web/src/main/java/ru/fastdelivery/presentation/calc/CalculateController.java
+++ web/src/main/java/ru/fastdelivery/presentation/calc/CalculateController.java
@@ -1,5 +1,9 @@
 package ru.fastdelivery.presentation.calc;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -18,11 +22,17 @@ import ru.fastdelivery.usecase.TariffCalculateUseCase;
 @RestController
 @RequestMapping("/api/v1/calculate/")
 @RequiredArgsConstructor
+@Tag(name = "Расчеты стоимости доставки")
 public class CalculateController {
     private final TariffCalculateUseCase tariffCalculateUseCase;
     private final CurrencyFactory currencyFactory;
 
     @PostMapping
+    @Operation(summary = "Расчет стоимости по упаковкам груза")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Successful operation"),
+            @ApiResponse(responseCode = "400", description = "Invalid input provided")
+    })
     public CalculatePackagesResponse calculate(@Valid @RequestBody CalculatePackagesRequest request) {
         var packsWeights = request.packages().stream()
                 .map(CargoPackage::weight)
